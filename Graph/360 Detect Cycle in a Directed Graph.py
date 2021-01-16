06bcf220ff87b3280ea9df32ba9194833dd9869e
Laptop@ikaushikpal <iamkaushik@Laptop.com>
2021-01-16 16:49:30 +0530

    added Detect Cycle in a Directed Graph

---
+++ Graph/360 Detect Cycle in a Directed Graph.py
@@ -0,0 +1,53 @@
+from collections import defaultdict
+
+
+class Graph:
+    def __init__(self):
+        self.graph = defaultdict(list)
+    
+    def addEdge(self, starting_vertex, end_vertex):
+        self.graph[starting_vertex].append(end_vertex)
+    
+    def isCyclic(self):
+        visitedVertices = defaultdict(bool)
+        recursionStack = defaultdict(bool)
+
+        FLAG = False
+
+        for u in list(self.graph):
+            if FLAG == True:
+                return True
+
+            if visitedVertices[u] == False:
+                FLAG |= self.dfsUtil(u, visitedVertices, recursionStack)  
+        return False
+
+
+    def dfsUtil(self, starting_vertex, visitedVertices, recursionStack):
+        recursionStack[starting_vertex] = True
+        visitedVertices[starting_vertex] = True
+
+
+        for vertex in self.graph[starting_vertex]:
+            if visitedVertices[vertex] == False:
+                res = self.dfsUtil(vertex, visitedVertices, recursionStack)
+                if res == True:
+                    return True
+                    
+            if recursionStack[vertex] == True:
+                return True
+        
+        recursionStack[starting_vertex] = False
+        return False
+
+
+g = Graph()
+g.addEdge('A','B')
+g.addEdge('B','C')
+g.addEdge('A','C')
+g.addEdge('B','D')
+g.addEdge('D','E')
+g.addEdge('E','B')
+
+res = g.isCyclic()
+print(res)
