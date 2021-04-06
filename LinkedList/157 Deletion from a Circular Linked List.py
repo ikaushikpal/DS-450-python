482f2bdbf3d73f27cb3014c7e4f06254b79baee3
Laptop@ikaushikpal <iamkaushik@Laptop.com>
2021-04-06 17:55:01 +0530

    solved linkedlist question

---
+++ LinkedList/157 Deletion from a Circular Linked List.py
@@ -0,0 +1,69 @@
+# Python program to delete a given key from
+# linked list.
+
+# Node of a doubly linked list
+class Node:
+	def __init__(self, next = None, data = None):
+		self.next = next
+		self.data = data
+
+# Function to insert a node at the beginning of
+# a Circular linked list
+def push(head_ref, data):
+
+	# Create a new node and make head as next
+	# of it.
+	ptr1 = Node()
+	ptr1.data = data
+	ptr1.next = head_ref
+
+	# If linked list is not None then set the
+	# next of last node
+	if (head_ref != None) :
+		
+		# Find the node before head and update
+		# next of it.
+		temp = head_ref
+		while (temp.next != head_ref):
+			temp = temp.next
+		temp.next = ptr1
+	
+	else:
+		ptr1.next = ptr1 # For the first node
+
+	head_ref = ptr1
+	return head_ref
+
+# Function to print nodes in a given
+# circular linked list
+def printList( head):
+
+	temp = head
+	if (head != None) :
+		while(True) :
+			print( temp.data, end = " ")
+			temp = temp.next
+			if (temp == head):
+				break
+	print()
+
+# Function to delete a given node from the list
+def deleteNode(head, key) :
+    
+
+
+head = None
+head = push(head, 2)
+head = push(head, 5)
+head = push(head, 7)
+head = push(head, 8)
+head = push(head, 10)
+
+print("List Before Deletion: ")
+printList(head)
+
+head = deleteNode(head, 7)
+
+print( "List After Deletion: ")
+printList(head)
+
