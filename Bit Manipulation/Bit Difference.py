720c89c4981e86f4a0b3ccc6a499748dfab00758
Desktop@ikaushikpal <iamkaushik2014@desktop.com>
2021-06-27 22:04:16 +0530

    Added more on bit manipulations

---
+++ Bit Manipulation/Bit Difference.py
@@ -0,0 +1,23 @@
+class Solution:
+    ##Complete this function
+    # Function to find number of bits needed to be flipped to convert A to B
+    def countBitsFlip(self,a,b):
+        counter = 0
+        while a and b:
+            if a&1 != b&1:
+                counter += 1
+
+            a >>= 1
+            b >>= 1
+
+        while a:
+            if a&1:
+                counter += 1
+            a >>= 1
+
+        while b:
+            if b&1:
+                counter += 1
+            b >>= 1
+
+        return counter 
